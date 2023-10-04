d9bf65367923c1af794d6bc55ed70c64356a9d92
Davi Nogueira <125682678+DaviNogueiraToledo@users.noreply.github.com>
2023-10-04 13:00:30 -0300

    Add files via upload

---
+++ update.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "phpmyadmin",
+  password: "aluno",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql  = "UPDATE customers SET address = 'Cayon 123' WHERE address = 'Valley 3455'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+});
+
+
